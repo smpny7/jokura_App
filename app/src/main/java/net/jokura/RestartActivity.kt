be9e41b77b01e9874ec1aeb7c5ecf9bacef4a6f8
smpny7 <symphonymode7@icloud.com>
2020-05-06 00:28:03 +0900

    restart デザイン概形

---
+++ app/src/main/java/net/jokura/RestartActivity.kt
@@ -0,0 +1,12 @@
+package net.jokura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restart)
+    }
+}
